377135bc919881357960f36788971e3cfd251454
jigneshchuadhary757 <jigneshchaudhary757@gmail.com>
2024-07-11 23:20:49 +0530

    changes in hooks and js file for to replace the client script

--- barcode_scanner/hooks.py
+++ barcode_scanner/hooks.py
@@ -32,6 +32,7 @@ app_license = "mit"
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+doctype_js = {"Purchase Receipt": "public/js/purchase_receipt.js"}
 
 # Svg Icons
 # ------------------
